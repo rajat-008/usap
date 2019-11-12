57b97b53d2443940689d9abed6e526536d152448
rajat-008 <mailrajat008@gmail.com>
2019-11-12 15:18:55 +0530

    first commit

--- webapp/models.py
+++ webapp/models.py
@@ -7,4 +7,11 @@ class Student(models.Model):
     dob=models.DateField()
     mail_id=models.EmailField()
     password=models.CharField(max_length=32)
+
+class degree_provider(models.Model):
+    deg_provider_id=models.AutoField(primary_key=True)
+    deg_provider_name=models.CharField(max_length=32)
+    mail_id=models.EmailField()
+    password=models.CharField(max_length=32)
+
 # Create your models here.
